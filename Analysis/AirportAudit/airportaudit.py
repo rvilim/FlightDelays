b967991a4da62394ff4eddb6a8d98f1dfe772c74
rvilim <ryan.vilim@gmail.com>
2015-03-03 16:30:55 -0500

    The Final Project

---
+++ Analysis/AirportAudit/airportaudit.py
@@ -0,0 +1,51 @@
+import matplotlib
+import pandas as pd
+import numpy as np
+import sys
+
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+years=sys.argv[1:]
+
+delays=pd.DataFrame()
+
+
+print 'Reading ...'
+for year in years:
+    delays=delays.append(pd.read_pickle('../../Data/delays_'+str(year)))
+
+print delays.dtypes
+
+delayedflights=delays[delays['ArrDelay']>=15]
+
+# print delayedflights
+A=delayedflights.groupby('DestAirportID').count().sort('Month')['Month']
+B=delays.groupby('DestAirportID').count().sort('Month')['Month']
+
+C=pd.concat([A,B],axis=1)
+
+C.to_csv('A.csv')
+# B=delays.groupby('DestAirportID').count().sort('Month').tail(15)
+
+# res = A.set_index('DestAirportID') / B.set_index('DestAirportID')
+
+
+
+#
+# # delays=delays[delays.OriginAirportID == jfk_airport_id]
+# # delays=delays[delays.AirlineID == AA_airline_id]
+#
+# delays=delays[(delays['Cancelled']==0)&(delays['Diverted']==0)]
+#
+# num_bins=30
+# bins = [-60,-45,-30,-15,0,15,30,45,60,75,90,105,120,135,150,165,180,195,210]
+#
+# n, bins, patches = plt.hist(delays['ArrDelay'].values, bins, normed=1, facecolor='grey', alpha=0.5,cumulative=True)
+# # Tweak spacing to prevent clipping of ylabel
+# plt.xlabel('Minutes late')
+# plt.ylabel('Normalized Histogram')
+# plt.ylim([0, 1])
+# plt.title('Arriving at SFO '+str(min(map(int,years)))+'-'+str(max(map(int,years))))
+# plt.subplots_adjust(left=0.15)
+# plt.savefig('SFO_cum_2010-2014.eps')
